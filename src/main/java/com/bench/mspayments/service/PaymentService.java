185c9224a3f25381fa729d0fdf83ff6452128b85
unknown <designanimation.inc@gmail.com>
2024-01-24 13:20:54 -0300

    feat: feature transfer and equeq

--- src/main/java/com/bench/mspayments/service/PaymentService.java
+++ src/main/java/com/bench/mspayments/service/PaymentService.java
@@ -1,18 +1,15 @@
 package com.bench.mspayments.service;
 
 import com.bench.mspayments.dto.*;
-import com.bench.mspayments.model.Account;
-import com.bench.mspayments.model.BankTransfer;
-import com.bench.mspayments.model.ECheck;
-import com.bench.mspayments.model.EMoney;
+import com.bench.mspayments.model.*;
+
+import java.util.List;
 import java.util.Optional;
 
 
 public interface PaymentService {
     public PaymentDTO filter(PaymentHistoryResponseDTO paymentHistoryResponseDTO);
     public Optional<Account> getAccount(Long accountSender);
-    public BankTransfer saveBankTransfer(BankTransferResponseDTO bankTransferResponseDTO);
-    public ECheck saveECheck(ECheckResponseDTO eCheckResponseDTO);
-    public EMoney saveEmoney(EMoneyResponseDTO eMoneyResponseDTO);
-
+    public BankTransfer saveBankTransfer(TransferRequestDTO transferRequestDTO);
+    public ECheck saveECheck(TransferRequestDTO transferRequestDTO);
 }
